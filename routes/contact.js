3eb4b0003113ab8f00c8f42aec952ca3c7057409
Jean-Cédric Huet <logit.pro@gmail.com>
2018-09-26 21:03:37 -0400

    Release 1

--- routes/contact.js
+++ routes/contact.js
@@ -1,28 +1,30 @@
-let router = require('express').Router();
+let express = require('express');
+let router = express.Router();
 let postmark = require("postmark");
-
+const POSTMARK_SERVERKEY = process.env.POSTMARK_SERVERKEY;
 // Send an email:
-let client = new postmark.Client(process.env.POSTMARK_SERVERKEY);
+let client = new postmark.Client(POSTMARK_SERVERKEY);
 
+router.post("/", function (req, res) {
 
-router.post('/', function (req, res) {
-  client.sendEmail({
-    "From": req.body.email,
-    "To": "tech@biscuitech.me",
-    "Subject": "New Message from Website: " + req.body.subject,
-    "TextBody": req.body.message
-  })
-    .then(client.sendEmail({
-      "From": "tech@biscuitech.me",
-      "To": req.body.email,
-      "Subject": "Message Reception Confirmation",
-      "TextBody": "This is an automated email to let you know that we have received your message. \n Please allow 48 business hours at most to process."
-      })
-    )
-    .catch(res => {
-      res.send("There seems to be an error with your message. Please")
-      throw
-    })
-
-})
+	console.log(req.body);
+	/* client.sendEmail({
+		"From": req.body.email,
+		"To": "tech@biscuitech.me",
+		"Subject": "New Message from Website: " + req.body.subject,
+		"TextBody": req.body.message
+	})
+		.then(client.sendEmail({
+			"From": "tech@biscuitech.me",
+			"To": req.body.email,
+			"Subject": "Message Reception Confirmation",
+			"TextBody": "This is an automated email to let you know that we have received your message. \n Please allow 48 business hours at most to process."
+		})
+		)
+		.catch(res => {
+			res.send("There seems to be an error with your message. Please");
+			throw new Error("PostMark Failed to deliver message");
+		}); */
+		res.send('200')
+});
 module.exports = router;
