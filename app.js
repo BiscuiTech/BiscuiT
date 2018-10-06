d191f672e9870765840b6312d4e78076b120439b
Jean-Cédric Huet <logit.pro@gmail.com>
2018-10-06 17:23:04 -0400

    Added Sentry.io + Page Swallow

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ let indexRouter = require('./routes/index');
 let contactRouter = require('./routes/contact');
 let app = express();
 
+// Error Tracking
+const Sentry = require("@sentry/node") ;
+Sentry.init({ dsn:"https://29038e9a86384a1e8a0d6a5c64753b35@sentry.io/1291180" });
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// The error handler must be before any other error middleware
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+	// The error id is attached to `res.sentry` to be returned
+	// and optionally displayed to the user for support.
+	res.statusCode = 500;
+	res.end(res.sentry + "\n");
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,6 +39,9 @@ app.use(favicon(path.join(__dirname, 'public', 'Biscui.png')))
 // routes definition
 app.use('/', indexRouter);
 app.use('/contact', contactRouter);
+app.get("*", function mainHandler(req, res, next) {
+	res.render("index");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
