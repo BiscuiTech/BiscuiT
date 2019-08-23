4153186ff5f5b295bf496b8ed542a0c46450fc9c
Jean-Cedric Huet <jhuet@biscui.tech>
2019-08-23 18:21:23 -0400

    Started work on v3 of the website

--- components/Socials.js
+++ components/Socials.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 const SocialsContainer = styled.div`
   display: flex;
-  right:0;
+  right: 0;
   position: fixed;
   flex-direction: row;
   margin: 18px 18px;
@@ -15,18 +15,25 @@ const SocialsContainer = styled.div`
   }
 `;
 
-export default class Socials extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <SocialsContainer>
-          <a href="https://www.facebook.com/biscuittech/" target="_blank"><img src="/static/facebook.svg" width="60px" height="60px"/></a>
-          <a href="https://ca.linkedin.com/in/jean-c%C3%A9dric-huet-7a0949a3" target="_blank"><img src="/static/linkedin.svg" width="60px" height="60px"/></a>
-          <a href="https://twitter.com/biscuitech" target="_blank"><img src="/static/twitter.svg" width="60px" height="60px"/></a>
-          {/* <a href="https://www.instagram.com/biscui.tech/" target="_blank"><img src="/static/instagram.svg" width="60px" height="60px"/></a> */}
-          <a href="https://github.com/biscuitech" target="_blank"><img src="/static/github.svg" width="60px" height="60px"/></a>
-        </SocialsContainer>
-      </React.Fragment>
-    )
-  }
-}
+const Socials = () => (
+  <SocialsContainer>
+    <a href="https://www.facebook.com/biscuittech/" target="_blank">
+      <img src="/static/facebook.svg" width="60px" height="60px" />
+    </a>
+    <a
+      href="https://ca.linkedin.com/in/jean-c%C3%A9dric-huet-7a0949a3"
+      target="_blank"
+    >
+      <img src="/static/linkedin.svg" width="60px" height="60px" />
+    </a>
+    <a href="https://twitter.com/biscuitech" target="_blank">
+      <img src="/static/twitter.svg" width="60px" height="60px" />
+    </a>
+    {/* <a href="https://www.instagram.com/biscui.tech/" target="_blank"><img src="/static/instagram.svg" width="60px" height="60px"/></a> */}
+    <a href="https://github.com/biscuitech" target="_blank">
+      <img src="/static/github.svg" width="60px" height="60px" />
+    </a>
+  </SocialsContainer>
+);
+
+export default Socials;
