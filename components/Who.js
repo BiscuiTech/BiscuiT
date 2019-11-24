dddfb9ed4e6ede1be1ed89f821e03d99faf93de0
biscuitech <jhuet@biscui.tech>
2019-11-24 09:08:55 -0500

    Reworded the /who block

--- components/Who.js
+++ components/Who.js
@@ -103,9 +103,9 @@ const Who = () => (
           Working as a Developer Analyst for a big Entertainment company in
           Montréal, I learned the ropes of the industry quickly with massive
           projects. Having a passion for cutting edge technology, I pushed my
-          own projects for the company to strive on. Now having my own name, set
-          of skills and talent, I am ready to give your web presence a touch of
-          modern, speed, reliabilty and performance.
+          own projects for the company to strive on. Now being a Freelance,
+          having my own set of skills and talent, I am ready to give your web
+          presence a touch of modern, speed, reliabilty and performance.
         </p>
         <p className="closing-words">
           Are you up for a revamp?{' '}
