c74752265137821389e4a28bf2a9d49afb4f5b94
biscuitech <jhuet@biscui.tech>
2019-11-25 11:02:04 -0500

    Fixed type on --vh

--- components/Layout.js
+++ components/Layout.js
@@ -98,14 +98,17 @@ const Layout = ({ title, description, url, ogImage, children }) => {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     const vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh - 50}px`);
+    document.documentElement.style.setProperty('--vh', `${Number(vh) - 50}px`);
     // We listen to the resize event
     window.addEventListener(
       'resize',
       debounce(() => {
         // We execute the same script as before
         const vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh - 50}px`);
+        document.documentElement.style.setProperty(
+          '--vh',
+          `${Number(vh) - 50}px`
+        );
       }, 400)
     );
   }
