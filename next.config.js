e0a107669335e72d8a1dc5ef99e8cfc3ad36e543
biscuitech <jhuet@biscui.tech>
2019-11-28 15:22:38 -0500

    Fixed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-module.exports = {
-  webpack: config => {
+const withSourceMaps = require('@zeit/next-source-maps')();
+
+module.exports = withSourceMaps({
+  webpack: (config, options) => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
       fs: 'empty',
     };
 
+    if (!options.isServer) {
+      config.resolve.alias['@sentry/node'] = '@sentry/browser';
+    }
     return config;
   },
-};
+});
