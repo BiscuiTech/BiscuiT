0330567e421e891da8d24684af8d22c14d31676b
biscuitech <jhuet@biscui.tech>
2020-01-22 15:17:03 -0500

    Fixed /what page props

--- pages/[lang]/what.tsx
+++ pages/[lang]/what.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
-const what = () => <Layout>what!</Layout>;
+const what = () => (<Layout
+  title="Biscui.Tech"
+  description="Biscui.Tech Home page"
+>what!</Layout>);
 
 export default what;
