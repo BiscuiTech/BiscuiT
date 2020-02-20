e19303783f1f79b9cd02bfe2f4c021adf86f87c7
biscuitech <jhuet@biscui.tech>
2020-02-20 15:21:18 -0500

    Finished /about

--- src/components/styles/Card.tsx
+++ src/components/styles/Card.tsx
@@ -20,9 +20,9 @@ const animation = () =>
     ${fadeInAnimation} 1s ease-in-out
 `;
 
-const CardStyles = styled.div`
+const BaseCardStyles = styled.div`
   width: 80%;
-  max-width: 500px;
+  max-width: ${props => props.maxWidth || '500px'};
   box-shadow: 4px 4px 6px rgba(0, 0, 0, 0.35);
   background: #FFFFFF;
   border-radius: 10px;
@@ -32,34 +32,39 @@ const CardStyles = styled.div`
   font-size: calc(18px + (24 - 18) * ((100vw - 300px) / (1600 - 300)));
   line-height: calc(24px + (30 - 24) * ((100vw - 300px) / (1600 - 300)));
   color: rgba(0, 0, 0, 0.7);
-  text-align: center;
+  text-align: ${props => props.textAlign || 'center'};
   position: relative;
   z-index: 5;
+  display: flex;
+  flex-direction: row;
+`
+
+const CardStylesWithAnimation = styled(BaseCardStyles)`
   transform: ${props => props.toggle ? null : 'translate3d(-50%,0,0)'};
   opacity: ${props => props.toggle ? 1 : 0};
   animation: ${props => props.toggle ? animation : null};
 `;
 
+
 type CardProps = {
   readonly children?: any,
   fadeIn?: boolean,
-  markup?: string
+  markup?: string,
+  maxWidth?: string,
+  textAlign?: string
 }
 
-function createMarkup(markup: string) {
-  return { __html: markup }
-}
-
-const Card = ({ children, fadeIn, markup }: CardProps) => {
+const Card = ({ children, fadeIn, markup, maxWidth, textAlign }: CardProps) => {
   const [toggle, setToggle] = useState(false);
-  // not animated
   if (!fadeIn) {
+    // not animated
     return (
       markup
-        ? <CardStyles toggle={toggle} dangerouslySetInnerHTML={{ __html: markup }} />
-        : <CardStyles toggle={toggle}>
+        ? <BaseCardStyles dangerouslySetInnerHTML={{ __html: markup }} maxWidth={maxWidth}
+          textAlign={textAlign} />
+        : <BaseCardStyles maxWidth={maxWidth} textAlign={textAlign}>
           {children}
-        </CardStyles>
+        </BaseCardStyles >
     )
   } else {
     // animated version
@@ -70,10 +75,10 @@ const Card = ({ children, fadeIn, markup }: CardProps) => {
           onEnter={() => setToggle(true)}
         />
         {markup
-          ? <CardStyles toggle={toggle} dangerouslySetInnerHTML={{ __html: markup }} />
-          : <CardStyles toggle={toggle}>
+          ? <CardStylesWithAnimation toggle={toggle} dangerouslySetInnerHTML={{ __html: markup }} maxWidth={maxWidth} textAlign={textAlign} />
+          : <CardStylesWithAnimation toggle={toggle} maxWidth={maxWidth} textAlign={textAlign}>
             {children}
-          </CardStyles>
+          </CardStylesWithAnimation>
         }
       </>
     )
--- src/translations/locales/fr/index.ts
+++ src/translations/locales/fr/index.ts
@@ -3,10 +3,12 @@ import contact from './contact'
 import common from './common'
 import home from './home'
 import blog from './blog'
+import about from './about'
 
 export default {
   contact,
   common,
   home,
-  blog
+  blog,
+  about
 }
