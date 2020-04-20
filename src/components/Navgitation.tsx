3407f9480327b3887f3f2cbdeeca0b3918b77dea
Jean-Cédric Huet <jhuet@biscui.tech>
2020-04-20 16:00:03 -0400

    Updated font in header

--- src/components/Navgitation.tsx
+++ src/components/Navgitation.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import Link from 'next/link';
-import useTranslation from '../hooks/useTranslation'
-import { useRouter } from 'next/router';
-import { motion, AnimateSharedLayout } from "framer-motion"
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import useTranslation from "../hooks/useTranslation";
+import { useRouter } from "next/router";
+import { motion, AnimateSharedLayout } from "framer-motion";
 
 const ActiveLinks = [
-  { tKey: 'navigation_Home', path: '' },
+  { tKey: "navigation_Home", path: "" },
   /* {tKey: 'navigation_Blog' , path: 'blog'}, */
-  { tKey: 'navigation_About', path: 'about' },
+  { tKey: "navigation_About", path: "about" },
   /* {tKey: 'navigation_Projects' , path: 'projects'}, */
-  { tKey: 'navigation_Contact', path: 'contact' },
-]
+  { tKey: "navigation_Contact", path: "contact" },
+];
 
 const NavgitationStyles = styled.nav`
   background: black;
@@ -19,23 +19,23 @@ const NavgitationStyles = styled.nav`
   bottom:0;
   width:100%;
   display:flex;
-  border-top: 1px solid ${props => props.theme.color.accent};
+  border-top: 1px solid ${(props) => props.theme.color.accent};
   border-bottom: none;
   z-index: 5;
   @media (min-width: 820px) {
     bottom: unset;
     top:0;
     width: 600px;
-    background: ${props => props.theme.background};
+    background: ${(props) => props.theme.background};
     border-top: none;
-    border-bottom: 1px solid ${props => props.theme.color.accent};
+    border-bottom: 1px solid ${(props) => props.theme.color.accent};
     border-radius: 5px;
     margin:auto;
     left: 50%;
     transform: translateX(-300px);
   }
   .nav-selected {
-    /* background: ${props => props.theme.color.accent}; */
+    /* background: ${(props) => props.theme.color.accent}; */
     background: black;
     height: 100%;
     width: 100%;
@@ -47,53 +47,43 @@ const NavgitationStyles = styled.nav`
 
 const StyledMenuLink = styled(motion.a)`
   font-size: 20px;
-  font-size: max(18px,max(2vw, 22px));
+  font-size: max(18px, min(2vw, 22px));
   padding: 12px 0;
-  color: ${props => props.current ? 'black' : 'white'};
+  color: ${(props) => (props.current ? "black" : "white")};
   cursor: pointer;
   flex-grow: 2;
   text-align: center;
-  font-weight: ${props => props.current ? '600' : '400'};
+  font-weight: ${(props) => (props.current ? "600" : "400")};
   position: relative;
 `;
 
 const Navigation = () => {
-  const { locale, t } = useTranslation()
+  const { locale, t } = useTranslation();
   const { pathname } = useRouter();
 
   const isCurrentPath = (path) => {
-    return pathname === `/[lang]${path}` || pathname === `/[lang]/${path}`
-  }
+    return pathname === `/[lang]${path}` || pathname === `/[lang]/${path}`;
+  };
 
   return (
     <AnimateSharedLayout>
       <NavgitationStyles>
         {ActiveLinks.map(({ tKey, path }, i) => (
-          <Link href={`/[lang]/${path}`} as={`/${locale}/${path}`}
-            key={i}
-
-          >
-            <StyledMenuLink
-              current={isCurrentPath(path)}
-              animate
-              key={i}
-            >
+          <Link href={`/[lang]/${path}`} as={`/${locale}/${path}`} key={i}>
+            <StyledMenuLink current={isCurrentPath(path)} animate key={i}>
               {isCurrentPath(path) && (
                 <motion.div
                   layoutId="nav-selected"
                   className="nav-selected"
-                  style={{ backgroundColor: '#FBB03B' }}
+                  style={{ backgroundColor: "#FBB03B" }}
                 />
               )}
-              {t('common')[tKey]}
+              {t("common")[tKey]}
             </StyledMenuLink>
           </Link>
         ))}
       </NavgitationStyles>
     </AnimateSharedLayout>
-  )
-}
-export default Navigation
-
-
-
+  );
+};
+export default Navigation;
