810b162bbbe00136035e508213acdcbef499d788
biscuitech <jhuet@biscui.tech>
2020-04-28 23:29:06 -0400

    Fix typo

--- src/translations/locales/fr/about.ts
+++ src/translations/locales/fr/about.ts
@@ -1,6 +1,6 @@
 const about = {
   header: "À propos de moi",
-  subHeader: "Appelle moi Jean-Cédric Huet",
+  subHeader: "Appelez-moi Jean-Cédric Huet",
   aboutMe: "En tant que développeur analyste pour une grande entreprise de divertissement à Montréal, j'ai rapidement appris les ficelles de l'industrie avec des projets massifs. Passionné par les technologies de pointe, j'ai poussé mes propres projets sur lesquels la société doit tendre. Désormais Freelance, ayant mes propres compétences et talents, je suis prêt à donner à votre présence sur le Web une touche de modernité, de vitesse, de fiabilité et de performance.",
   contactMe: "Rentrez en contact ici."
 }
