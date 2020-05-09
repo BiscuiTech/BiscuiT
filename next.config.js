725bd6ec3d048242105a78338751114b065fe9ce
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-08 21:06:23 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,38 +1,45 @@
 const withSourceMaps = require('@zeit/next-source-maps')();
 const TerserPlugin = require('terser-webpack-plugin');
-
-module.exports = withSourceMaps({
-  target: 'serverless',
-  webpack: (config, options) => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty',
-    };
-
-    if (!options.isServer) {
-      config.resolve.alias['@sentry/node'] = '@sentry/browser';
-      config.resolve.alias['react-dom$'] = 'react-dom/profiling';
-      config.resolve.alias['scheduler/tracing'] = 'scheduler/tracing-profiling';
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            keep_classnames: true,
-            keep_fnames: true,
-          },
-        }),
-      ];
-    }
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    });
-
-    return config;
-  },
-  experimental: {
-    async rewrites() {
-      return [{ source: '/sitemap.xml', destination: '/api/sitemap.xml' }];
-    },
-    catchAllRouting: true,
-  },
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
 });
+
+module.exports = withMDX(
+  withSourceMaps({
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    target: 'serverless',
+    webpack: (config, options) => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty',
+      };
+
+      if (!options.isServer) {
+        config.resolve.alias['@sentry/node'] = '@sentry/browser';
+        config.resolve.alias['react-dom$'] = 'react-dom/profiling';
+        config.resolve.alias['scheduler/tracing'] =
+          'scheduler/tracing-profiling';
+        config.optimization.minimizer = [
+          new TerserPlugin({
+            terserOptions: {
+              keep_classnames: true,
+              keep_fnames: true,
+            },
+          }),
+        ];
+      }
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      });
+
+      return config;
+    },
+    experimental: {
+      async rewrites() {
+        return [{ source: '/sitemap.xml', destination: '/api/sitemap.xml' }];
+      },
+      catchAllRouting: true,
+    },
+  })
+);
--- src/pages/[lang]/blog/index.tsx
+++ src/pages/[lang]/blog/index.tsx
@@ -1,48 +1,51 @@
-import React from 'react'
-import Layout from '../../../components/Layout'
-import BlogList from '../../../components/BlogList'
-import { GetStaticProps, GetStaticPaths } from 'next'
-import { getLocalizationProps, LanguageProvider } from '../../../context/LanguageContext'
-import matter from 'gray-matter'
+import React from "react";
+import Layout from "../../../components/Layout";
+import BlogList from "../../../components/BlogList";
+import { GetStaticProps, GetStaticPaths } from "next";
+import {
+  getLocalizationProps,
+  LanguageProvider,
+} from "../../../context/LanguageContext";
+import matter from "gray-matter";
 
 const BlogIndexPage = ({ localization, posts, preview = false }) => {
   /**
    * TODO: Preview mode
    */
-  const publishedPosts = arr => arr.filter(el => el.frontmatter.published == 'true');
-  console.log(publishedPosts(posts))
+  const publishedPosts = (arr) =>
+    arr.filter((el) => el.frontmatter.published == "true");
+  console.log(publishedPosts(posts));
   return (
     <LanguageProvider localization={localization}>
-      <Layout
-        title="Biscui.Tech"
-        description="Biscui.Tech Home page"
-      >
+      <Layout title="Biscui.Tech" description="Biscui.Tech Home page">
         <BlogList posts={posts} />
       </Layout>
     </LanguageProvider>
-  )
-}
+  );
+};
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const posts = ((context) => {
-    const keys = context.keys()
-    const values = keys.map(context)
+    const keys = context.keys();
+    const values = keys.map(context);
 
     const data = keys.map((key, index) => {
-      let slug = key.replace(/^.*[\\\/]/, '').slice(0, -3)
-      const value = values[index]
-      const document = matter(value.default)
+      let slug = key.replace(/^.*[\\\/]/, "").slice(0, -3);
+      const value = values[index];
+      // console.log("value: ", value.default());
+      const document = matter(value.default());
       return {
         frontmatter: document.data,
         // markdownBody: document.content,
         slug,
-      }
-    })
-    return data
+      };
+    });
+    console.log("data: ", data);
+    return data;
     //@ts-ignore
-  })(require.context('../../../blog', true, /\.md$/))
+  })(require.context("../../../blog", true, /\.md$/));
 
-  const localization = getLocalizationProps(ctx, 'blog');
+  const localization = getLocalizationProps(ctx, "blog");
   return {
     props: {
       posts,
@@ -58,4 +61,4 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export default BlogIndexPage
+export default BlogIndexPage;
