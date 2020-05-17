619a03e268a0a11c2e7ce135ac2f402f32614d18
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-16 21:13:43 -0400

    fixed typo

--- src/translations/locales/fr/home.ts
+++ src/translations/locales/fr/home.ts
@@ -1,9 +1,10 @@
 const home = {
-  welcome_msg: 'Salut, mon nom est',
-  i_am: 'Je suis un développeur web localisé à Montréal.',
-  short_intro: "Je crée d'excellents sites web basé sur des outils modernes tels que React et GraphQL.",
+  welcome_msg: "Salut, mon nom est",
+  i_am: "Je suis un développeur web localisé à Montréal.",
+  short_intro:
+    "Je crée d'excellents sites web basés sur des outils modernes tels que React et GraphQL.",
   latestBlog: "Dernier Blog",
-  viewBlog: "Lire cet article"
-}
+  viewBlog: "Lire cet article",
+};
 
-export default home
+export default home;
