7ad8b9267b98f4d8194d9ed8a30d9b1cafc5b3dd
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-18 11:02:29 -0400

    wip

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,5 +1,26 @@
-import React from 'react'
-import useAlert from '../hooks/useAlert';
+import React, { useState, useEffect } from "react";
+
+export enum EAlert {
+  "INFO",
+  "WARNING",
+  "ERROR",
+  "SUCCESS",
+}
+
+interface IAlert {
+  type: EAlert;
+  isOpen: boolean;
+  message: string;
+}
+
+const useAlert = () => {
+  const [alert, setAlert] = useState<IAlert>({
+    type: EAlert.INFO,
+    isOpen: false,
+    message: "",
+  });
+  return { alert, setAlert };
+};
 
 const Alerts = () => {
   const { alert } = useAlert();
@@ -9,8 +30,16 @@ const Alerts = () => {
         <div className="rounded-md bg-green-50 p-4">
           <div className="flex">
             <div className="flex-shrink-0">
-              <svg className="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+              <svg
+                className="h-5 w-5 text-green-400"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                />
               </svg>
             </div>
             <div className="ml-3">
@@ -21,8 +50,16 @@ const Alerts = () => {
             <div className="ml-auto pl-3">
               <div className="-mx-1.5 -my-1.5">
                 <button className="inline-flex rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:bg-green-100 transition ease-in-out duration-150">
-                  <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  <svg
+                    className="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    />
                   </svg>
                 </button>
               </div>
@@ -30,8 +67,8 @@ const Alerts = () => {
           </div>
         </div>
       </div>
-    )
-  } else return <></>
-}
+    );
+  } else return <></>;
+};
 
 export default Alerts;
