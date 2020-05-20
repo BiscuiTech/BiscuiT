fba6241dd4d00d33172c52d8c3f58a7aa3e05f56
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-20 16:59:20 -0400

    Reworked TechPills and /uses

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
-import { ConfiguredTech } from '../lib/projects'
+import React from "react";
+import styled from "styled-components";
+
+export const ConfiguredTech = [
+  { name: "apollo", id: "apollo", background: "#FFFFFF", color: "#000000" },
+  { name: "graphql", id: "graphql", background: "#E00098", color: "#FFFFFF" },
+  { name: "react", id: "react", background: "#60D9FB", color: "#FFFFFF" },
+  { name: "prisma", id: "prisma", background: "#0C344B", color: "#FFFFFF" },
+  {
+    name: "postgresql",
+    id: "postgresql",
+    background: "#0071BC",
+    color: "#FFFFFF",
+  },
+  { name: "next.js", id: "nextjs", background: "#000000", color: "#FFFFFF" },
+];
 
 const TechStackStyles = styled.div`
-  transform: ${props => `translate3d(0,-${props.imageHeight + 'px'},0)`};
+  transform: ${(props) => `translate3d(0,-${props.imageHeight + "px"},0)`};
   width: 100%;
-  height:100%;
+  height: 100%;
   h4 {
     font-family: Montserrat;
     font-style: normal;
@@ -20,53 +33,48 @@ const TechStackStyles = styled.div`
     display: flex;
     padding: 0 12px;
   }
-  li {
-    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.35);
-    border-radius: 5px;
-    font-family: 'Source Code Pro';
-    font-size: 14px;
-    line-height: 18px;
-    text-align: center;
-    text-transform: lowercase;
-    flex: none;
-    order: 0;
-    align-self: center;
-    margin: 6px;
-    padding: 2px 6px;
-  }
 `;
 
-const TechPill = styled.li`
-  background: ${props => `${props.styles.background}`};
-  color: ${props => `${props.styles.color}`};
+export const TechPill = styled.li`
+  background: ${(props) => `${props.styles.background}`};
+  color: ${(props) => `${props.styles.color}`};
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.35);
+  border-radius: 5px;
+  font-family: "Source Code Pro";
+  line-height: 18px;
+  text-align: center;
+  text-transform: lowercase;
+  flex: none;
+  order: 0;
 `;
 
-function getTechStyles(id: string) {
-  const payload = ConfiguredTech.filter(el => {
-    return el.id === id
-  })
-  return payload[0]
+export function getTechStyles(id: string) {
+  const payload = ConfiguredTech.filter((el) => {
+    return el.id === id;
+  });
+  return payload[0];
 }
 
 const TechStack = ({ imageHeight, info }) => {
   return (
     <TechStackStyles imageHeight={imageHeight}>
-      <h4>
-        Tech Stack
-      </h4>
+      <h4>Tech Stack</h4>
       <ul>
         {info.map((el, i) => {
           const styles = getTechStyles(el);
           return (
-            <TechPill key={i} styles={styles}>
+            <TechPill
+              key={i}
+              styles={styles}
+              className="self-start py-1 px-2 m-2"
+            >
               {styles.name}
             </TechPill>
-          )
-        })
-        }
+          );
+        })}
       </ul>
     </TechStackStyles>
-  )
-}
+  );
+};
 
-export default TechStack
+export default TechStack;
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,95 +1,92 @@
 export enum ETechStack {
-  'apollo' = 'apollo',
-  'graphql' = 'graphql',
-  'react' = 'react',
-  'prisma' = 'prisma',
-  'postgresql' = 'postgresql',
-  'next.js' = 'nextjs'
+  "apollo" = "apollo",
+  "graphql" = "graphql",
+  "react" = "react",
+  "prisma" = "prisma",
+  "postgresql" = "postgresql",
+  "next.js" = "nextjs",
 }
 
 interface IProject {
-  title: string,
-  description: string,
-  imageUrl: string,
-  imageAlt: string,
-  websiteLink: string,
-  techStack: ETechStack[],
+  title: string;
+  description: string;
+  imageUrl: string;
+  imageAlt: string;
+  websiteLink: string;
+  techStack: ETechStack[];
 }
-
-export const ConfiguredTech = [
-  { name: 'apollo', id: 'apollo', background: '#FFFFFF', color: '#000000' },
-  { name: 'graphql', id: 'graphql', background: '#E00098', color: '#FFFFFF' },
-  { name: 'react', id: 'react', background: '#60D9FB', color: '#FFFFFF' },
-  { name: 'prisma', id: 'prisma', background: '#0C344B', color: '#FFFFFF' },
-  { name: 'postgresql', id: 'postgresql', background: '#0071BC', color: '#FFFFFF' },
-  { name: 'next.js', id: 'nextjs', background: '#000000', color: '#FFFFFF' },
-]
 
 const projects: IProject[] = [
   {
-    title: 'EquipeValcourt.com',
-    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!',
-    imageUrl: '',
-    imageAlt: 'Lorem ipsum dolor sit amet',
-    websiteLink: 'https://equipevalcourt.com',
+    title: "EquipeValcourt.com",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!",
+    imageUrl: "",
+    imageAlt: "Lorem ipsum dolor sit amet",
+    websiteLink: "https://equipevalcourt.com",
     techStack: [
       ETechStack.apollo,
       ETechStack["next.js"],
       ETechStack.graphql,
-      ETechStack.postgresql
-    ]
+      ETechStack.postgresql,
+    ],
   },
   {
-    title: 'EquipeValcourt.com',
-    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!',
-    imageUrl: '',
-    imageAlt: 'Lorem ipsum dolor sit amet',
-    websiteLink: 'https://equipevalcourt.com',
+    title: "EquipeValcourt.com",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!",
+    imageUrl: "",
+    imageAlt: "Lorem ipsum dolor sit amet",
+    websiteLink: "https://equipevalcourt.com",
     techStack: [
       ETechStack.apollo,
       ETechStack["next.js"],
       ETechStack.graphql,
-      ETechStack.postgresql
-    ]
+      ETechStack.postgresql,
+    ],
   },
   {
-    title: 'EquipeValcourt.com',
-    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!',
-    imageUrl: '',
-    imageAlt: 'Lorem ipsum dolor sit amet',
-    websiteLink: 'https://equipevalcourt.com',
+    title: "EquipeValcourt.com",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!",
+    imageUrl: "",
+    imageAlt: "Lorem ipsum dolor sit amet",
+    websiteLink: "https://equipevalcourt.com",
     techStack: [
       ETechStack.apollo,
       ETechStack["next.js"],
       ETechStack.graphql,
-      ETechStack.postgresql
-    ]
-  }, {
-    title: 'EquipeValcourt.com',
-    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!',
-    imageUrl: '',
-    imageAlt: 'Lorem ipsum dolor sit amet',
-    websiteLink: 'https://equipevalcourt.com',
+      ETechStack.postgresql,
+    ],
+  },
+  {
+    title: "EquipeValcourt.com",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!",
+    imageUrl: "",
+    imageAlt: "Lorem ipsum dolor sit amet",
+    websiteLink: "https://equipevalcourt.com",
     techStack: [
       ETechStack.apollo,
       ETechStack["next.js"],
       ETechStack.graphql,
-      ETechStack.postgresql
-    ]
-  }
-  , {
-    title: 'EquipeValcourt.com',
-    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!',
-    imageUrl: '',
-    imageAlt: 'Lorem ipsum dolor sit amet',
-    websiteLink: 'https://equipevalcourt.com',
+      ETechStack.postgresql,
+    ],
+  },
+  {
+    title: "EquipeValcourt.com",
+    description:
+      " Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut adipisci fuga consequuntur amet nulla fugiat deleniti eos qui neque, deserunt exercitationem labore eaque iste nobis corporis, illo tenetur ab rem!",
+    imageUrl: "",
+    imageAlt: "Lorem ipsum dolor sit amet",
+    websiteLink: "https://equipevalcourt.com",
     techStack: [
       ETechStack.apollo,
       ETechStack["next.js"],
       ETechStack.graphql,
-      ETechStack.postgresql
-    ]
-  }
-]
+      ETechStack.postgresql,
+    ],
+  },
+];
 
-export default projects
+export default projects;
