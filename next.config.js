772fc023eb4f7af0d315c2991f57578f4330cf88
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-21 07:01:36 -0400

    Updated sentry tracking

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withPlugins = require('next-compose-plugins');
 const withSourceMaps = require('@zeit/next-source-maps')();
 const TerserPlugin = require('terser-webpack-plugin');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
@@ -8,8 +9,17 @@ const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
 });
 
+const {
+  NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
+  SENTRY_ORG,
+  SENTRY_PROJECT,
+  SENTRY_AUTH_TOKEN,
+  NODE_ENV,
+} = process.env;
+process.env.SENTRY_DSN = SENTRY_DSN;
+
 module.exports = withPlugins(
-  [[withBundleAnalyzer], [withSourceMaps], [withMDX]],
+  [[SentryWebpackPlugin], [withBundleAnalyzer], [withSourceMaps], [withMDX]],
   {
     target: 'serverless',
     webpack: (config, options) => {
@@ -17,7 +27,20 @@ module.exports = withPlugins(
       config.node = {
         fs: 'empty',
       };
-
+      // In `pages/_app.js`, Sentry is imported from @sentry/node. While
+      // @sentry/browser will run in a Node.js environment, @sentry/node will use
+      // Node.js-only APIs to catch even more unhandled exceptions.
+      //
+      // This works well when Next.js is SSRing your page on a server with
+      // Node.js, but it is not what we want when your client-side bundle is being
+      // executed by a browser.
+      //
+      // Luckily, Next.js will call this webpack function twice, once for the
+      // server and once for the client. Read more:
+      // https://nextjs.org/docs#customizing-webpack-config
+      //
+      // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+      // building the browser's withBundleAnalyzer
       if (!options.isServer) {
         config.resolve.alias['@sentry/node'] = '@sentry/browser';
         config.resolve.alias['react-dom$'] = 'react-dom/profiling';
@@ -31,6 +54,21 @@ module.exports = withPlugins(
         //     },
         //   }),
         // ];
+      }
+      if (
+        SENTRY_DSN &&
+        SENTRY_ORG &&
+        SENTRY_PROJECT &&
+        SENTRY_AUTH_TOKEN &&
+        NODE_ENV === 'production'
+      ) {
+        config.plugins.push(
+          new SentryWebpackPlugin({
+            include: '.next',
+            ignore: ['node_modules'],
+            urlPrefix: '~/_next',
+          })
+        );
       }
       config.module.rules.push({
         test: /\.mdx?$/,
