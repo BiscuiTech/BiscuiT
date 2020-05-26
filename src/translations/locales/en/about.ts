7d48f7332a21c48973e40cfd362e3d5bec3a8d89
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-26 17:16:35 -0400

    Fixed alt image

--- src/translations/locales/en/about.ts
+++ src/translations/locales/en/about.ts
@@ -1,9 +1,11 @@
 const about = {
   pageHeader: "About myself",
   subHeader: "Call me Jean-Cédric Huet",
-  aboutMe: 'Working as a Developer Analyst for a big Entertainment company in Montréal, I learned the ropes of the industry quickly with massive projects. Having a passion for cutting edge technology, I pushed my own projects for the company to strive on. Now being a Freelance, having my own set of skills and talent, I am ready to give your web presence a touch of modern, speed, reliabilty and performance.',
-  position: 'Web Developer',
-  contactMe: "Get in touch right here."
-}
+  aboutMe:
+    "Working as a Developer Analyst for a big Entertainment company in Montréal, I learned the ropes of the industry quickly with massive projects. Having a passion for cutting edge technology, I pushed my own projects for the company to strive on. Now being a Freelance, having my own set of skills and talent, I am ready to give your web presence a touch of modern, speed, reliabilty and performance.",
+  position: "Web Developer",
+  contactMe: "Get in touch right here.",
+  profilePictureAlt: "Profile picture of Jean-Cédric Huet",
+};
 
-export default about
+export default about;
