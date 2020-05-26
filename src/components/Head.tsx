39f5fb107aedbd10a2af6194094a0ed283bf6446
Jean-Cédric Huet <jhuet@biscui.tech>
2020-05-26 16:55:52 -0400

    Fixed contact form email

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -9,7 +9,7 @@ const defaultOGImage = "";
 const Head = ({ title, description, og }) => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta
       name="viewport"
       content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5"
