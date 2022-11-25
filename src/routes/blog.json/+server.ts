a10a764a69c724a67ed95162f59741dbdd7938d9
Jean-Cédric Huet <jhuet@biscui.tech>
2022-11-25 18:23:58 -0500

    wwip

--- src/routes/blog.json/+server.ts
+++ src/routes/blog.json/+server.ts
@@ -2,8 +2,6 @@
 import { getPosts } from '$lib/Blog/api';
 
 export function GET() {
-  console.log("GETTING POSTS");
-  console.log(getPosts());
   return new Response(JSON.stringify({
     body: getPosts(),
   }));
