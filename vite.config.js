d11e2d503b0ec3a0e3ab2f156a06c4594f0ebfb4
Jean-Cédric Huet <jhuet@biscui.tech>
2025-08-26 23:54:34 -0400

    feat: upgraded deps

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,22 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { imagetools } from 'vite-imagetools';
-import { BundlePrismjs } from "rollup-plugin-prismjs";
-
+import { BundlePrismjs } from 'rollup-plugin-prismjs';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  logLevel: 'info',
-  plugins: [imagetools(), sveltekit(), BundlePrismjs({
-    languages: [
-      "shell",
-      "tsx",
-      "typescript",
-      "javascript",
-      "markup",
-      "css"
-    ],
-    css: true,
-    theme: "dracula"
-  })],
-  server: {
-    fs: {
-      strict: false
-    }
-  },
-
+	logLevel: 'info',
+	plugins: [
+		tailwindcss(),
+		imagetools(),
+		sveltekit(),
+		BundlePrismjs({
+			languages: ['shell', 'tsx', 'typescript', 'javascript', 'markup', 'css'],
+			css: true,
+			theme: 'dracula'
+		})
+	],
+	server: { fs: { strict: false } }
 };
 
 export default config;
